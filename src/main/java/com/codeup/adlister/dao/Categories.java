63afb05bdd8cade907f5dc13a63a3b5f8a746883
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-01-17 11:31:12 -0600

    Interfaces

--- src/main/java/com/codeup/adlister/dao/Categories.java
+++ src/main/java/com/codeup/adlister/dao/Categories.java
@@ -1,4 +1,16 @@
 package com.codeup.adlister.dao;
+import com.codeup.adlister.models.Category;
+import com.codeup.adlister.models.Planet;
+
+import java.util.List;
 
 public interface Categories {
+    //function to display all categories related to the planets
+    List<Category> all();
+
+    //function to add a category
+    Long insert(Category category);
+
+    //function to display planets with respective categories
+    List<Planet> combined(Planet planet);
 }
